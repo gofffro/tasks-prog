3b7ff43ba6f7621ab07bce9424d97ef3ae19e8bc
gofffro <vazheninstepan@gmail.com>
2025-01-25 08:41:12 +0700

    task08.cpp

---
+++ task08.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int rows = 0, cols = 0;
+    cout << "Введите количество строк матрицы: ";
+    cin >> rows;
+    cout << "Введите количество столбцов матрицы: ";
+    cin >> cols;
+
+    if (rows < 0 || cols < 0) {
+        cout << "Неправильные размеры матрицы!" << endl;
+        return 1;
+    }
+    int matrix[rows][cols];
+    for (int rowsMatrix = 0; rowsMatrix < rows; ++rowsMatrix) {
+        for (int colsMatrix = 0; colsMatrix < cols; ++colsMatrix) {
+            cout << "Элемент(" << rowsMatrix + 1 << "," << colsMatrix + 1 << "): ";
+            cin >> matrix[rowsMatrix][colsMatrix];
+        }
+    }
+
+    int transposedMatrix[cols][rows];
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            transposedMatrix[j][rows - 1 - i] = matrix[i][j];
+        }
+    }
+
+    cout << "Исходная матрица:" << endl;
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    cout << "Транспонированная матрица:" << endl;
+    for (int i = 0; i < cols; ++i) {
+        for (int j = 0; j < rows; ++j) {
+            cout << transposedMatrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+ 
